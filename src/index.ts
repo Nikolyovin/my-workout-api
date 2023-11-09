e02710feecf62105dceed8ed1465dd5cdcf1dd8f
Лёвин Николай Сергеевич <151705@grpz.su>
2023-11-09 16:39:17 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { weightRouter } from './routers/weight-router'
 import mongoose from 'mongoose'
 import { authRouter } from './routers/auth-router'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 5200
@@ -10,6 +11,12 @@ const DB_URL = 'mongodb://localhost:27017/my-workout'
 app.use(express.json()) //нужно тк express по дефолту не понимает json
 app.use('/weight', weightRouter)
 app.use('/auth', authRouter)
+const corsOptions = {
+    origin: '*',
+    credentials: true //access-control-allow-credentials:true
+    // optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 async function startApp() {
     try {
